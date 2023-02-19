d3c01140cf881b5f2d36632ae99f08d1d0a9efb7
Daviid7505 <davidrodriguez_7@hotmail.com>
2023-02-19 20:40:46 +0100

    David javadoc

---
+++ Entornos/David/Javadoc_JUnit_David/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"javabeans","l":"Jugador"},{"p":"javabeans","l":"Soldado"},{"p":"javabeans","l":"Testing"}];updateSearchResults();

46e78da0fe6c104bb815e6b0418b2dd0b1fdd831
alberto.saboya <albertosaboya@gmail.com>
2023-03-01 21:55:14 +0100

    reparacion proyecto eclipse

--- PROGRAMACION AD_7/ad7_Grupo_04/src/modelo/daojdbc/DepartamentoDao.java
+++ PROGRAMACION AD_7/ad7_Grupo_04/src/modelo/daojdbc/DepartamentoDao.java
@@ -3,7 +3,6 @@ package modelo.daojdbc;
 import java.util.List;
 
 import modelo.javabeans.Departamento;
-<<<<<<< HEAD
 
 public interface DepartamentoDao {
 
@@ -16,16 +15,5 @@ public interface DepartamentoDao {
 	Departamento buscarUnDepartamento(int idDepartamento);
 
 	List<Departamento> buscarTodosDepartamentos();
-=======
-import modelo.javabeans.Perfil;
-
-public interface DepartamentoDao {
-	
-	int altaDepartamento(Departamento departamento);
-	int eliminarUno (int idDepartamento);
-	int modificarUno(Departamento departamento); 
-	List<Perfil> buscarTodos();
-	Departamento buscarUno (int idDepartamento);
-
 
 }
